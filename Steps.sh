d1e011f9a91267c51d3d37e28ada50fcf0787f5c
Manohar Shetty <maddy98989@gmail.com>
2026-01-12 22:14:08 +0530

    Update Steps.sh

--- Steps.sh
+++ Steps.sh
@@ -1,6 +1,31 @@
+#Configure Sonarqube
 Tools -> Dependency->DP-Check->installation->automatic
 Tools => SonarQube Scanner installations =>Add Sonarqube Scanner =>name=sonar-server
 Systems->SonarQube installations->Name->sonar-server->Server authentication token->Sonar-token
 Manage Jenkins → Configure System → SonarQube=> SonarQube installations=>  Name=> sonar-server => Server URL =>http://65.2.184.54:9000=> Server authentication token => Add - Select a  token or create a new token-> Sonarqube URL-> Administration-Configuration-> Security -> User -> Create a new token - Take token -> and create a secret in jenkins.
 Administration=>Security=>Users=>create a token=>Sonar-token
 Administration-Configuration->Webhooks->Create->Name->jenkins-sonar-webhoook->URL->http://65.2.137.252:8080/sonarqube-webhook
+
+#Setting sudo Permission for docker
+sudo chmod 666 /var/run/docker.sock
+sudo usermod -aG docker jenkins
+sudo systemctl restart docker
+sudo systemctl restart jenkins
+
+# installation of Snyk
+# Install Node.js if not already installed
+sudo apt update
+sudo apt install -y nodejs npm
+# Install Snyk using npm
+npm install -g snyk
+
+# Download the latest Snyk CLI binary
+curl -Lo snyk "https://static.snyk.io/cli/latest/snyk-linux"
+chmod +x snyk
+sudo mv snyk /usr/local/bin
+snyk --version
+
+#Trivy installlation
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin
+trivy --version
+
