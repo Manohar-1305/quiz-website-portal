7719cc1a0c2849d7290f98134e8b9bb730f997a2
Manohar Shetty <maddy98989@gmail.com>
2026-01-12 14:24:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 
 class Config:
     SECRET_KEY = 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'mysql://quiz_user:password@15.206.72.196/quizdb'  # Ensure this is set before SQLAlchemy initialization
+    SQLALCHEMY_DATABASE_URI = 'mysql://quiz-user:password@15.206.72.196/quizdb'  # Ensure this is set before SQLAlchemy initialization
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Optionally disable modification tracking
+
