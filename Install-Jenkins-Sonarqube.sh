0095752e6e13ecfdedfcb59aa481bcffdd0b88d0
Manohar Shetty <maddy98989@gmail.com>
2026-01-13 12:23:55 +0530

    Add Java and Jenkins installation to script
    Added installation steps for Java and Jenkins to the script.

--- Install-Jenkins-Sonarqube.sh
+++ Install-Jenkins-Sonarqube.sh
@@ -1,1 +1,23 @@
 
+#Install Java
+sudo apt update
+sudo apt install -y openjdk-21-jdk
+
+# Install Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt install -y jenkins
+
+# Enable & check Status
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+
+# Install sonarqube
