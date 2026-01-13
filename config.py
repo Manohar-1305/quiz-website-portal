065bb68695cd081565a7e2f06f55fb5a9cdf8db6
Manohar Shetty <maddy98989@gmail.com>
2026-01-13 21:42:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 class Config:
     SECRET_KEY = 'supersecretkey'
     # Use the Kubernetes service name for MySQL
-    SQLALCHEMY_DATABASE_URI = 'mysql://quiz-user:password@13.126.169.108:3306/quizdb'
+    SQLALCHEMY_DATABASE_URI = 'mysql://quiz-user:password@mysql-service:3306/quizdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
